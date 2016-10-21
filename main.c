f5983c0afc18da49648ad1d84abb0a8e70b07ec6
Taxouck <taxouck1@live.fr>
2016-10-21 16:25:07 +0200

    kj

--- main.c
+++ main.c
@@ -5,13 +5,30 @@
 
 int main()
 {
-    int longueur = 0;
+
+    /*
+    entier: long, larg, i, j
+
+    début
+    lire long
+    lire larg
+    pour i allant de 0 à long-1 faire
+        pour j allant de 0 à larg-1 faire
+        si (i=0 ou i=long-1 ou j=0 ou j=larg-1) alors écrire "#" sinon écrire "*"
+        fin pour
+    nouvelle ligne
+    fin pour
+
+    fin
+    */
+
+    int longueur = 0;  //Les deux int que l'utilisateur va pouvoir manipuler
     int largeur = 0;
-    int i=0;
+    int i=0;  //Les deux int nécessaires pour les boucles
     int j=0;
 
     printf("Entrez la longueur\n");
-    scanf("%i",&longueur);
+    scanf("%i",&longueur);          //L'utilisateur entre les deux valeurs qu'il souhaite utiliser pour son carré
     printf("Entrez la largeur\n");
     scanf("%i",&largeur);
 
@@ -19,7 +36,7 @@ int main()
             for(j=0;j<longueur;j++){
                     if(i==0 || i==longueur-1 || j==0 || j==largeur-1){
                         printf("#");
-                    } else {
+                    } else {        //L'ordinateur créée le carré selon les valeurs rentrées
                         printf("*");
                     }
             }
